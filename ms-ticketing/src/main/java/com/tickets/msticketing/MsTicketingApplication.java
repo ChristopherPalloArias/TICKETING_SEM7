44a0b24a544e6fe5786696d863d79d2afb7b38b5
Maese-Alfred <luis_pinzon82232@elpoli.edu.co>
2026-03-26 08:11:52 -0500

    feat: Initialize ms-ticketing project with Gradle and Spring Boot

---
+++ ms-ticketing/src/main/java/com/tickets/msticketing/MsTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.tickets.msticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTicketingApplication.class, args);
+    }
+
+}
---
+++ ms-ticketing/src/test/java/com/tickets/msticketing/MsTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickets.msticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
