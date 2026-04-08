4fe5bde0126193f5c69741400f9bfcf84ab3496f
ChristopherPalloArias <cipallo@uce.edu.ec>
2026-04-08 04:04:14 -0500

    refactor: simplify CarteleraPage by removing unused filters and updating event handling

--- frontend/src/pages/CarteleraPage/CarteleraPage.tsx
+++ frontend/src/pages/CarteleraPage/CarteleraPage.tsx
@@ -1,47 +1,25 @@
 import NavBar from '../../components/NavBar/NavBar';
 import BottomNav from '../../components/NavBar/BottomNav';
-import FilterBar from '../../components/FilterBar/FilterBar';
 import EventGrid from '../../components/EventGrid/EventGrid';
 import HeroCarousel from '../../components/HeroCarousel/HeroCarousel';
 import { useEvents } from '../../hooks/useEvents';
-import { useEventFilters } from '../../hooks/useEventFilters';
 import styles from './CarteleraPage.module.css';
 
 export default function CarteleraPage() {
   const { events, loading, loadingMore, error, hasMore, loadMore } = useEvents();
-  const {
-    filteredEvents,
-    searchQuery,
-    setSearchQuery,
-    tierFilter,
-    setTierFilter,
-    dateFilter,
-    setDateFilter,
-  } = useEventFilters(events);
 
   return (
     <div className={styles.page}>
       <NavBar activeLink="eventos" isTransactional={false} />
 
       {/* HeroCarousel: todos los eventos, edge-to-edge */}
-      {!loading && filteredEvents.length > 0 && (
-        <HeroCarousel events={filteredEvents} />
+      {!loading && events.length > 0 && (
+        <HeroCarousel events={events as any} />
       )}
 
       <main className={styles.main}>
-        <section className={styles.filters}>
-          <FilterBar
-            searchQuery={searchQuery}
-            onSearch={setSearchQuery}
-            tierFilter={tierFilter}
-            onTierChange={setTierFilter}
-            dateFilter={dateFilter}
-            onDateChange={setDateFilter}
-          />
-        </section>
-
         <EventGrid
-          events={filteredEvents}
+          events={events as any}
           loading={loading}
           loadingMore={loadingMore}
           error={error}
